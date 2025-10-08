b5d9b9c5cfc37bc08d4ea7784ede7d8ff665c482
karuppaiya <karuppu29032005@gmail.com>
2025-10-08 20:50:59 +0530

    second commit

---
+++ family.py
@@ -0,0 +1,8 @@
+myname=input("your name:")
+fathername=input("your father name:")
+mothername=input("your mother name:")
+bothername=input("your bother name:")
+
+print(myname,"\n",fathername,"\n",mothername,"\n",bothername,"\n")
+
+    

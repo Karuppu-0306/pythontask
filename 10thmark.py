7bd45ab21b3c47041234e3ca56e2246db6018e22
karuppaiya <karuppu29032005@gmail.com>
2025-10-08 20:52:04 +0530

    third commit

---
+++ 10thmark.py
@@ -0,0 +1,10 @@
+tamil=int(input("enter your tamil mark"))
+english=int(input("enter your english mark"))
+maths=int(input("enter your maths mark"))
+senices=int(input("enter your senices mark"))
+socialsenices=int(input("enter your social senicces mark"))
+total=tamil+english+maths+senices+socialsenices
+
+print(tamil,"\n",english,"\n",maths,"\n",senices,"\n",socialsenices,"\n",total)
+
+

fe2dd8603ce656d4df0d980421b41dd218ebb4a4
karuppaiya <karuppu29032005@gmail.com>
2025-10-08 20:53:53 +0530

    fifth commit

---
+++ formula.py
@@ -0,0 +1,7 @@
+import math
+r=int(input("enter the radius:"))
+h=int(input("enter the helight:"))
+print("circle",math.pi*r**2)
+print("cylinder",math.pi*r**2*h)
+print("spherek",math.pi*r**2*4/3)
+print("cone",math.pi*r**3*1/3)

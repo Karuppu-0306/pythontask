007c616124cb550e2048f9ad4cd21b4151017ab6
karuppaiya <karuppu29032005@gmail.com>
2025-10-08 20:52:58 +0530

    fourth commit

---
+++ 12thmark.py
@@ -0,0 +1,11 @@
+tamil=int(input("enter your tamil mark:"))
+english=int(input("enter your english mark:"))
+accounts=int(input("enter your accounts mark:"))
+commerce=int(input("enter your commerce mark:"))
+histroy=int(input("enter your history mark:"))
+economice=int(input("enter your economice mark:"))
+total=tamil+english+accounts+commerce+histroy+economice
+
+print(tamil,"\n",english,"\n",accounts,"\n",commerce,"\n",histroy,"\n",economice,"\n",total)
+
+
